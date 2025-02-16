c18421a54e8f71c6513b0d0fe7d49b567057f927
yuanwang <wang881@ucbs.edu>
2025-02-16 08:26:52 +0000

    second try answer for partd

--- 129section3/TuringMachine.py
+++ 129section3/TuringMachine.py
@@ -3,22 +3,48 @@ class TuringMachine:
         self.tape = list(f"{A}#{B}#0")
         self.head = 0
         self.state = "init"
-        self.steps = []
-        self.step_count = 0 
-        self.max_steps = 100000  
+        self.step_count = 0
+        self.max_steps = 100_000  # Prevent infinite loops
 
     def transition(self):
+        """Execute state transitions until halt or max_steps."""
         while self.state != "halt" and self.step_count < self.max_steps:
-            self.steps.append("".join(self.tape))
-            self.step_count += 1  
-        return self.steps
+            # Ensure head is within tape bounds (expand tape if needed)
+            if self.head < 0:
+                self.head = 0  # Reset to start
+            elif self.head >= len(self.tape):
+                self.tape.append('0')  # Expand tape to the right
 
-def save_tape(A, B):
-    tm = TuringMachine(A, B)
-    steps = tm.transition()
-    filename = f"{A}_{B}.dat"
-    with open(filename, "w") as f:
-        f.write("\n".join(steps))
+            current_symbol = self.tape[self.head]
+
+            # State transition logic (example)
+            if self.state == "init":
+                if current_symbol == '#':
+                    self.state = "read_B"
+                self.head += 1
+
+            elif self.state == "read_B":
+                if current_symbol == '1':
+                    self.state = "add_A"
+                elif current_symbol == '#':
+                    self.state = "halt"  # End of B
+                self.head += 1
+
+            elif self.state == "add_A":
+                self.tape[self.head] = 'X'  # Mark processed bit
+                self.state = "shift_A"
+                self.head += 1
+
+            elif self.state == "shift_A":
+                self.head -= 1  # Move back to A
+                if self.head < 0:  # Prevent underflow
+                    self.head = 0
+                self.state = "read_B"
+
+            self.step_count += 1
+
+        return self.step_count
+
 if __name__ == "__main__":
     save_tape("101001010111", "101000101")
     save_tape("101111", "101001")
